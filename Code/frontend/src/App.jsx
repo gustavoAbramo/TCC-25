1180b81e825878f751ffc580c4bd47dd5312e938
GUSTAVO HENRIQUE GONÇALVES ABRAMO <025019@fatecararas.com>
2025-08-11 16:24:59 -0300

    feat: adicionei o tailwind e criei algumas rotas

--- Code/frontend/src/App.jsx
+++ Code/frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
+import AppRoutes from "./appRoutes"
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <AppRoutes />
   )
 }
 
--- Code/frontend/src/appRoutes.jsx
+++ Code/frontend/src/appRoutes.jsx
@@ -1,1 +1,24 @@
 //aqui vai conter as rotas de cada pagina do nosso sistema e dps nos so importamos elas no main.jsx
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
+
+
+
+export default function AppRoutes() {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
+        </BrowserRouter>
+    );
+    }
+
+//aqui vai conter as rotas de cada pagina do nosso sistema e dps nos so importamos elas no main.jsx
