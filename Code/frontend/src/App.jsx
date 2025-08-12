75fe6544b3deef5f6f79215eb93be7c199f2fada
Bruno <brunoro343@gmail.com>
2025-08-12 14:37:38 -0300

    tela de login adicionada + algumas novas pastas para estilos, a tela de login e o index

--- Code/frontend/src/App.jsx
+++ Code/frontend/src/App.jsx
@@ -1,13 +1,54 @@
-import { useState } from 'react'
+import React from 'react';
+import './styles/styleMain.css';
+import logo from './assets/logo-safeplay.png';
+import './styles/globalsMain.css';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>hello world</h1>
-    </>
-  )
+    
+    <div className="page">
+      <div className="logo"><img src={logo} alt='logo' className='logo-padrao'/></div>
+
+      <div className="frame-37968">
+        <div className="links">
+          <div className="j-tem-uma-conta-entrar">
+            <span className="j-tem-uma-conta-entrar-span">Já tem uma conta?</span>
+            <span className="j-tem-uma-conta-entrar-span2">Entrar</span>
+          </div>
+        </div>
+        <div className="Headline">Bem vindo</div>
+      </div>
+
+      <form
+        className="form-main"
+        onSubmit={(e) => {
+          e.preventDefault();
+          // handle submit logic
+        }}
+      >
+        <label className="label-nome">
+          <span className="label">Nome do usuário</span>
+          <input className="text-field" name="username" />
+        </label>
+
+        <label className="label-email">
+          <span className="label">Email</span>
+            <input className="text-field" type="email" name="email" />
+        </label>
+
+        <label className="label-senha">
+          <span className="label">Senha</span>
+          <input className="text-field" type="password" name="password" />
+        </label>
+
+        <div className="login frame-276">
+          <button type="submit" className="btnlogin">
+            Enviar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Code/frontend/src/appRoutes.jsx
+++ Code/frontend/src/appRoutes.jsx
@@ -1,1 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
+
 //aqui vai conter as rotas de cada pagina do nosso sistema e dps nos so importamos elas no main.jsx
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* Adicione outras rotas aqui, se necessário */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
